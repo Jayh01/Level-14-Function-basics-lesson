be1ed0d9d7a51676b61c283e8e48255784abf201
Jayh01 <84378798+Jayh01@users.noreply.github.com>
2021-08-09 17:13:22 +0200

    Add files via upload

---
+++ Level-14-Function-basics-lesson/index.js
@@ -0,0 +1,10 @@
+const joop = function () {
+    console.log('boil water')
+    console.log('ad spagetty')
+    console.log('kook 5 min')
+    console.log('drain water spagetty')
+    console.log('remove spaggetty')
+};
+
+joop();
+
